2c48c5f862420204d3f3721fc8cd720df88b05f1
doanh dao <quangdoanh04072004@gmail.com>
2024-09-23 22:17:14 +0700

    Tạo project-fe-2

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+tailwind.config = {
+  theme: {
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1240px',
+      '2xl': '1240px',
+    },
+    extend: {
+      colors: {
+        main: '#000000',
+        second_main: '#00000099'
+      }
+    },
+  }
+}
